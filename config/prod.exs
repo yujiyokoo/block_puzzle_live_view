515fff00fb93b48c038b5c1ebd9c4b3e11e08f5c
Yuji Yokoo <yujiyokoo@gmail.com>
2020-01-27 11:14:36 +0900

    Update host config to use env vars

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :block_puzzle_live_view, BlockPuzzleLiveViewWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: {:system, "HOST"}, port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
